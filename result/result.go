8ae411250e04c608841153e5a602137e3d4e9ef8
root <rohits@heaptrace.com>
2019-04-10 22:08:17 +0530

    Updated responseWriter with json

--- result/result.go
+++ result/result.go
@@ -12,8 +12,9 @@ func WriteErrorResponse(responseWriter http.ResponseWriter, err error) {
 }
 
 func WriteJsonResponse(responseWriter http.ResponseWriter, bytes []byte, statusCode int) {
-	responseWriter.WriteHeader(statusCode)
 	responseWriter.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	responseWriter.WriteHeader(statusCode)
+
 	_, err := responseWriter.Write(bytes)
 	if err != nil {
 		log.Fatal(err)
